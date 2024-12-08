e8c9d694bbeb32b58f1e29650f6a7faf5896a427
Aweb Barouni <awebbarouni@gmail.com>
2024-12-08 23:14:23 +0100

    final

--- main.c
+++ main.c
@@ -4,17 +4,32 @@
 #include <string.h>
 #include <stdbool.h>
 
+
 int main(){
     int choix;
     FILE* FDrive,*FIDriver,*FDepense,*FIDepense,*FVoiture,*FIVoiture;
-    FACTURE tarifs=saisirTarifs();
+    FACTURE tarifs;
     VOITURE* voiture;DEPENSES* depenses;DRIVER* drive;
+
     int nbrDepenses,NbrDriver,nbVoiture;
     /*creerFichierDriver(&FDrive,&FIDriver);
     int nc=saisirEntier("le nombre des chauffeurs dans le parking");
     DRIVER* drv=creerTabDriver(nc);
     remplirTabDriver(drv,nc,tarifs,FDrive,FIDriver);
-    afficherTabDriver(drv,nc);*/
+    afficherTabDriver(drv,nc);
+    creerFichierDepenses(&FDepense,&FIDepense);
+    printf("Voulez-vous savoir les depenses dans combien de jours ? \n");
+    scanf("%d",&nbrDepenses);
+    depenses=allocTabDep(nbrDepenses);
+    remplirTabDepenses(depenses, nbrDepenses,FDepense,FIDepense);
+    afficherLesDepenses(depenses,nbrDepenses);
+    /*creerFichierVoiture(&FVoiture,&FIVoiture);
+    nbVoiture=saisirEntier("le nombre des voitures dans le parking");
+    VOITURE* v=creerTabVoiture(nbVoiture);
+    remplirFichierVoiture(v,nbVoiture,FVoiture,FIVoiture,tarifs);
+    printf("jjjjjjjjjjjjjjjjjjjj");
+    afficherTabVoiture(v,nbVoiture);*/
+
 
     system("color 04");
     do {
@@ -30,23 +45,18 @@ int main(){
         switch (choix) {
             case 1:
                 system("color 05");
-                OpenFichierDriver(&FDrive,&FIDriver);
-                afficherFichierIndex(FIDriver);
-                NbrDriver=NombreDeElemetnFichierIndex(FIDriver);
-                printf("dddddddddddddddd %d",NbrDriver);
-                drive=creerTabDriver(NbrDriver);
-                copierFichierDriverDansTableau(FDrive,FIDriver,drive,NbrDriver);
-                afficherTabDriver(drive,NbrDriver);
-                //Chargement(&FDrive,&FIDriver,&FDepense,&FIDepense,&FVoiture,&FIVoiture,&voiture,&depenses,&drive,&NbrDriver,&nbrDepenses,&nbVoiture);
-                //afficherMenuPrincipal(voiture,nbVoiture,drive, NbrDriver, tarifs,depenses,nbrDepenses);
+                Chargement(&FDrive,&FIDriver,&FDepense,&FIDepense,&FVoiture,&FIVoiture,&voiture,&depenses,&drive,&NbrDriver,&nbrDepenses,&nbVoiture);
+                printf("%d",NbrDriver);
+                afficherMenuPrincipal(&voiture,&nbVoiture,&drive, &NbrDriver, tarifs,&depenses,&nbrDepenses);
                 break;
             case 2:
                 system("color 01");
-                //Initialiser();
+                Initialiser(FDrive,FIDriver,FDepense,FIDepense,FVoiture,FIVoiture,tarifs,voiture,depenses,drive,nbrDepenses,NbrDriver,nbVoiture);
                 break;
             case 3:
                 printf("\n Merci");
                 break;
+
             default:
                 printf("\nErreur : Choix invalide. Veuillez reessayer.\n");
         }
